8a5348b64d0f8d78a40cec2d551bda6bfab6c6c4
Vidhi <mathurvidhi2505@gmail.com>
2024-08-02 20:55:32 +0530

    Styling for hiding scrollbar visibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
       theme: {
         extend: {},
       },
-      plugins: [],
+      plugins: [
+        function ({addUtilities}) {
+            const newUtility = {
+                ".hide-scrollbar::-webkit-scrollbar": {
+                 display: "none"
+                },
+                ".hide-scrollbar": {
+                    "-ms-overflow-style": "none",
+                    "scrollbar-width": "none"
+                  }
+            }
+            addUtilities(newUtility)
+        }
+      ],
   }
   
