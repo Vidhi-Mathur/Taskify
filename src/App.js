2255a2d87c6c252f6d14a83b89c2c5ad73d351cb
Vidhi <mathurvidhi2505@gmail.com>
2024-08-09 20:46:21 +0530

    Wrapped Task Context Provider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import { HomePage } from "./components/Pages/HomePage";
 import { TaskDetailPage } from './components/Pages/TaskDetailPage';
 import { Layout } from './components/UI/Layout';
+import { TaskCtxProvider } from './store/Task-Context';
 
 function App() {
   return (
-    <Layout>
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/:taskId" element={<TaskDetailPage />} />
-        </Routes>
-    </Layout>
+    <TaskCtxProvider>
+        <Layout>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/:taskId" element={<TaskDetailPage />} />
+            </Routes>
+        </Layout>
+    </TaskCtxProvider>
   );
 }
 
