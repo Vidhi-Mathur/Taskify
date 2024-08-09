d014b1b6b5bc3d5868b76e96a51435788c461f76
Vidhi <mathurvidhi2505@gmail.com>
2024-08-09 20:47:45 +0530

    Comment added

--- src/components/UI/ErrorDialog.js
+++ src/components/UI/ErrorDialog.js
@@ -1,3 +1,5 @@
+//Dialog to display error message
+
 export const ErrorDialog = ({error, onClose}) => {
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
